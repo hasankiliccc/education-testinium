23f8b4a166ff0b533d29eb844394aa3df0a0d549
hasankilic <hasan.kilic@testinium.com>
2025-11-03 14:47:57 +0300

    login with password and username

--- education-testinium/src/Main.java
+++ education-testinium/src/Main.java
@@ -1,16 +1,38 @@
-
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helsdsladsdso and welcwsasdsdasdastatusome!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lütfen kullanıcı adınızı giriniz :");
+        String username = scanner.nextLine();
+
+        System.out.println("Lütfen şifrenizi giriniz :");
+        String password = scanner.nextLine();
+        if(username.equals("admin") && password.equals("1234")){
+            System.out.println("Hoşgeldin admin ");
+        }else{
+            System.out.println("Giriş başarısız");
+        }
+
+      /*  System.out.println("Helsdsladsdso and welcwsasdsdasdastatusome!");
 
         int hasan = 23 ;
         int hüseyin = 10 ;
 
 
+
+        System.out.println("hasan : " + hasan);
+
+
+        int[] isimler = {1,2,3,4,5,6,7,8,9,33};
+
+
         for (int i = 0; i < hüseyin; i++) {
             System.out.println("i = sadas" + i);
-        }
+            System.out.println("isimler = " + isimler[i]);
+
+        }*/
     }
 }
