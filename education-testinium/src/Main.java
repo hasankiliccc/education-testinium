bf2c2375a2179223cdfa38c470e3a032ff9216bc
hasankilic <hasan.kilic@testinium.com>
2025-11-03 14:11:35 +0300

    java öğrenimi

--- education-testinium/src/Main.java
+++ education-testinium/src/Main.java
@@ -1,13 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Helsdsladsdso and welcwsasdsdasdastatusome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = sadas" + i);
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Helsdsladsdso and welcwsasdsdasdastatusome!");
+
+        int hasan = 23 ;
+        int hüseyin = 10 ;
+
+
+        for (int i = 0; i < hüseyin; i++) {
+            System.out.println("i = sadas" + i);
+        }
     }
 }
